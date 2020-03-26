73a88104897d7d1dc3b62eab87253ea48292fc7f
Oleg Duginov <oduginov@gmail.com>
2020-03-26 18:42:56 +0300

    add Caesar's cypher

--- my_caesar_cli.js
+++ my_caesar_cli.js
@@ -1,8 +1,9 @@
-// Import an encryption/decryption module
-const cryptoSystem = require('./src/caesar_cypher.js');
+// Import an encryption/decryption function
+const {cryptoSystem} = require('./src/caesar_cypher.js');
 
 // Obtaining the console arguments
 const argv = require('minimist')(process.argv.slice(2));
 
-
 console.log(argv);
+
+console.log(cryptoSystem("AbC", 26, 'encryption'));
--- src/caesar_cypher.js
+++ src/caesar_cypher.js
@@ -1,14 +1,38 @@
 const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+const ENCRYPTION = 'encryption';
 
-function encrypt(plainText){
+/**
+ * Encrypt/decrypt a given text by the Caesar's cypher.
+ *
+ * @param {string} text - A piece of a text, which we would like to encrypt or to decrypt.
+ * @param {number} shift - The parameter of the Caesar's cypher.
+ * @param {string} mode - If mode = 'encryption', then we encrypt the text; otherwise we decrypt the text
+ */
+const cryptoSystem = (text, shift, mode = ENCRYPTION) => {
+    if (mode !== ENCRYPTION) {
+        shift *= -1;
+    }
+    return text.split('').map(symbol => {
+        const lowerCaseOfSymbol = symbol.toLowerCase();
 
-}
-
-function decrypt(cypherText){
-
-}
-
-module.exports = {
-    encrypt: encrypt,
-    decrypt: decrypt,
+        // find an index of the symbol in the array <alphabet>,
+        // if the symbol is an english letter.
+        const index = alphabet.indexOf(lowerCaseOfSymbol);
+        if (index >= 0) {
+            const isLowerCase = symbol === lowerCaseOfSymbol; // whether the symbol is in the lower registry
+            let shiftedIndex = (index + shift) % alphabet.length;
+            if (shiftedIndex < 0) {
+                shiftedIndex += alphabet.length;
+            }
+            let image = alphabet[shiftedIndex];
+            if (!isLowerCase) {
+                image = image.toUpperCase();
+            }
+            return image;
+        } else {
+            return symbol;
+        }
+    }).join('');
 };
+
+module.exports = {cryptoSystem};
